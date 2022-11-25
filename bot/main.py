7ab161346333b2a167ccc0fcf1c1f17cc558f67b
Gribstovich <Gribstovich@proton.me>
2022-11-26 01:54:18 +0500

    Create a file to run the bot

---
+++ bot/main.py
@@ -0,0 +1,39 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from config import load_config, check_env_variables
+from database import create_db
+from handlers import register_admin_handlers, register_user_handlers
+
+
+def register_all_handlers(dp: Dispatcher):
+    register_admin_handlers(dp)
+    register_user_handlers(dp)
+
+
+async def main():
+    check_env_variables()
+    config = load_config()
+
+    storage = MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(bot, storage=storage)
+
+    register_all_handlers(dp)
+    create_db()
+
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        pass
