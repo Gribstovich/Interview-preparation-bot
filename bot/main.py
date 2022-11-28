a28f64acf608a636f01747d4bed352e9d2957998
Gribstovich <Gribstovich@proton.me>
2022-11-28 05:03:57 +0500

    Create a register_all_filters() function

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,12 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import load_config, check_env_variables
 from database import create_db
+from filters import IsAdmin
 from handlers import register_admin_handlers, register_user_handlers
+
+
+def register_all_filters(dp: Dispatcher):
+    dp.filters_factory.bind(IsAdmin)
 
 
 def register_all_handlers(dp: Dispatcher):
@@ -16,12 +21,13 @@ def register_all_handlers(dp: Dispatcher):
 async def main():
     check_env_variables()
     config = load_config()
-
     storage = MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp = Dispatcher(bot, storage=storage)
 
+    register_all_filters(dp)
     register_all_handlers(dp)
+
     create_db()
 
     try:
