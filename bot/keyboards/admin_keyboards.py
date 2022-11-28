a935876fb378cad0daf06fd16e5765019dcad5c6
Gribstovich <Gribstovich@proton.me>
2022-11-29 01:13:09 +0500

    Create the create_admin_edit_kb() function

--- bot/keyboards/admin_keyboards.py
+++ bot/keyboards/admin_keyboards.py
@@ -4,6 +4,15 @@ from .create_universal_keyboard import create_universal_kb
 
 
 async def create_admin_main_kb() -> ReplyKeyboardMarkup:
-    markup = await create_universal_kb(button_names=['edit tasks', 'newsletter', 'user mode', 'help'],
+    markup = await create_universal_kb(button_names=['edit tasks', 'newsletter',
+                                                     'user mode', 'help'],
+                                       row_width=2)
+    return markup
+
+
+async def create_admin_edit_kb() -> ReplyKeyboardMarkup:
+    markup = await create_universal_kb(button_names=['create category', 'del category',
+                                                     'create task', 'del task',
+                                                     'step back'],
                                        row_width=2)
     return markup
