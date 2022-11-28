a057a3c4d0fa2b14922e061a5751f7d1aba13a3d
Gribstovich <Gribstovich@proton.me>
2022-11-28 05:03:08 +0500

    Create start_admin_command() function
    This function is called if the user sent /start and his user_id is specified in the ADMIN_IDS environment variable

---
+++ bot/handlers/admin/start_admin_command.py
@@ -0,0 +1,12 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+
+from bot.config.locales import get_help_message_content
+from bot.keyboards.admin import create_admin_main_kb
+
+
+async def start_admin_command(message: Message, state: FSMContext) -> None:
+    text = await get_help_message_content('start_admin')
+    markup = await create_admin_main_kb()
+    await message.answer(text, reply_markup=markup)
+    await state.finish()
