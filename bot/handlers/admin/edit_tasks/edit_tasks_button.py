5e54a0ae678f5b2a2f2d70fc13cf222d168d65c4
Gribstovich <Gribstovich@proton.me>
2022-12-11 17:57:36 +0500

    Upgrade and rename the admin_edit_tasks_button() function

--- bot/handlers/admin/edit_tasks/edit_tasks_button.py
+++ bot/handlers/admin/edit_tasks/edit_tasks_button.py
@@ -3,10 +3,11 @@ from aiogram.types import Message
 
 from bot.config.locales import get_help_message_content
 from bot.keyboards import create_admin_edit_kb
+from bot.states.admin_states import Edit
 
 
-async def admin_edit_tasks_button(message: Message, state: FSMContext) -> None:
+async def edit_tasks_button(message: Message, state: FSMContext) -> None:
     text = await get_help_message_content('select action')
     markup = await create_admin_edit_kb()
     await message.answer(text, reply_markup=markup)
-    # something with state
+    await state.set_state(Edit.waiting_for_action.state)
